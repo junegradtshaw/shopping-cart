35de27920011c51496fe7f8ac64316d22f7e9e9e
June Shaw <june.shaw@me.com>
2016-02-25 21:37:35 -0700

    index partial and main controller

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
-var app = angular.module('shopping-cartApp', ['ngRoute'])
-        app.config(function($routeProvider) {
-            $routeProvider
-                .when('/', {
-                    templateUrl: '../partials/index.html',
-                    controller: 'MainController'
-                })
-                .otherwise({redirectTo : '/'})
+var app = angular.module('shoppingCartApp', ['ngRoute'])
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '../partials/index.html',
+            controller: 'MainController'
         })
+        .otherwise({redirectTo : '/'})
+})
---
+++ js/controllers/main_controller.js
@@ -0,0 +1,7 @@
+app.controller('MainController', ['$scope', function($scope){
+      $scope.test='HTML and Routes Working'
+      // $scope.categories=[{name:'awesome'}, {name:'cold'}]
+      // $scope.categories=['awesome', 'cold', 'warm', 'hot', 'winter', 'spring', 'summer', 'fall', 'dark', 'lucid']
+      // $scope.sortChoices=['Price', 'cold', 'warm', 'hot', 'winter', 'spring', 'summer', 'fall', 'dark', 'lucid']
+      // $scope.sortPrice=[{direction: 'Highest', descend: 'true'}, {direction: 'Lowest', descend: 'false'}]
+    }])
