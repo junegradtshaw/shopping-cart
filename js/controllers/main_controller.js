667a7a8f7dcf000c2be525d71726fff12d303856
June Shaw <june.shaw@me.com>
2016-02-25 22:14:15 -0700

    index table

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -1,7 +1,12 @@
 app.controller('MainController', ['$scope', function($scope){
-      $scope.test='HTML and Routes Working'
-      // $scope.categories=[{name:'awesome'}, {name:'cold'}]
-      // $scope.categories=['awesome', 'cold', 'warm', 'hot', 'winter', 'spring', 'summer', 'fall', 'dark', 'lucid']
+      $scope.test='HTML and Routes Working';
+      // $scope.categories=['awesome', 'cold'];
+      $scope.categories=[{name:'awesome'}, {name:'cold'}, {name: 'warm'}, {name: 'hot'}, {name: 'winter'},
+      {name: 'spring'}, {name: 'summer'}, {name: 'fall'}, {name: 'dark'}, {name: 'lucid'}];
+      $scope.selectedCategory=$scope.categories[0];
+      // $sort.choices=[{name: 'cold'}, {name: 'warm'}, {name: 'hot'}, {name: 'winter'}, {name: 'spring'}, {name: 'summer'}, 'fall', 'dark', 'lucid']}]
+      //
       // $scope.sortChoices=['Price', 'cold', 'warm', 'hot', 'winter', 'spring', 'summer', 'fall', 'dark', 'lucid']
-      // $scope.sortPrice=[{direction: 'Highest', descend: 'true'}, {direction: 'Lowest', descend: 'false'}]
+      $scope.sortPrices=[{name: 'Highest', descend: 'true'}, {name: 'Lowest', descend: 'false'}];
+      $scope.selectedSortPrice = $scope.sortPrices[0];
     }])
