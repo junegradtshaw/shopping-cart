ca230c841ddacc13ac4ecc7216ed12709792e263
June Shaw <june.shaw@me.com>
2016-02-26 07:39:27 -0700

    fixed table head page 1

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -3,10 +3,9 @@ app.controller('MainController', ['$scope', function($scope){
       // $scope.categories=['awesome', 'cold'];
       $scope.categories=[{name:'awesome'}, {name:'cold'}, {name: 'warm'}, {name: 'hot'}, {name: 'winter'},
       {name: 'spring'}, {name: 'summer'}, {name: 'fall'}, {name: 'dark'}, {name: 'lucid'}];
-      $scope.selectedCategory=$scope.categories[0];
-      // $sort.choices=[{name: 'cold'}, {name: 'warm'}, {name: 'hot'}, {name: 'winter'}, {name: 'spring'}, {name: 'summer'}, 'fall', 'dark', 'lucid']}]
+      $scope.searchCategory=$scope.categories[0];
       //
-      // $scope.sortChoices=['Price', 'cold', 'warm', 'hot', 'winter', 'spring', 'summer', 'fall', 'dark', 'lucid']
       $scope.sortPrices=[{name: 'Highest', descend: 'true'}, {name: 'Lowest', descend: 'false'}];
       $scope.selectedSortPrice = $scope.sortPrices[0];
+      $scope.searchName;
     }])
